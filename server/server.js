d6a6be74be1c925851b8646525549bb887add4b0
howmehow <howmehow@gmail.com>
2020-05-07 12:49:36 +0100

    Preparing app for heroku part 2

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ if(process.env.NODE_ENV === 'production'){
   //Static folder
   app.use(express.static(__dirname + '/public/'));
   //Handle SPA
-  app.get(/.*/);
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
 
