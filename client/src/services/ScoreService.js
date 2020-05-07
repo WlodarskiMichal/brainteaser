fa50bd44ff4a1dfc35e0bbe0ca7a74130cdc55cc
howmehow <howmehow@gmail.com>
2020-05-07 12:48:26 +0100

    Preparing app for Heroku

--- client/src/services/ScoreService.js
+++ client/src/services/ScoreService.js
@@ -1,4 +1,4 @@
-const baseURL = 'http://localhost:3000/api/scores/'
+const baseURL = '/api/scores/'
 export default {
 
 
--- server/server.js
+++ server/server.js
@@ -5,10 +5,22 @@ const bodyParser = require('body-parser');
 const cors = require("cors");
 
 app.use(cors())
-
+//Middleware
 app.use(bodyParser.json());
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router.js');
+
+const scores = require('./routes/api/scores')
+
+app.use('/api/scores', scores)
+//Handle production
+if(process.env.NODE_ENV === 'production'){
+  //Static folder
+  app.use(express.static(__dirname + '/public/'));
+  //Handle SPA
+  app.get(/.*/);
+}
+
 
 MongoClient.connect('mongodb://localhost:27017')
   .then((client) => {
